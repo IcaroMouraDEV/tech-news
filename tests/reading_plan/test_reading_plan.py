c16f715ee6a6f633eb2efc2591c9c1c9ae68220f
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-13 21:14:51 -0300

    feat: implement req 6

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,19 @@
 from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tests.assets.news import NEWS
+import pytest
 
 
-def test_reading_plan_group_news():
-    pass
+def mock_data():
+    return NEWS
+
+
+def test_reading_plan_group_news(mocker):
+    mocker.patch('tech_news.analyzer.reading_plan.find_news', mock_data)
+
+    data = ReadingPlanService.group_news_for_available_time(3)
+
+    assert len(data['readable']) == 4
+    assert len(data['unreadable']) == 4
+
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(-1)
