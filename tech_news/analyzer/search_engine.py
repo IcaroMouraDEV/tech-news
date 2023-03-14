355c0bdd77394f60b153fb5cff886e79c090ede0
Icaro Joel <icaromoura.programador@gmail.com>
2023-03-13 21:25:51 -0300

    feat: implement req 7 and 8 and 9

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,13 +1,35 @@
+from tech_news.database import search_news
+from datetime import datetime
+
 # Requisito 7
 def search_by_title(title):
     """Seu código deve vir aqui"""
+    return [
+        (new['title'], new['url']) for new in search_news(
+            {"title": {"$regex": title, "$options": "i"}}
+        )
+    ]
 
 
 # Requisito 8
 def search_by_date(date):
     """Seu código deve vir aqui"""
+    try:
+        date = datetime.fromisoformat(date).strftime("%d/%m/%Y")
 
+        return [
+            (new['title'], new['url']) for new in search_news(
+                {"timestamp": date}
+            )
+        ]
+    except ValueError:
+        raise ValueError('Data inválida')
 
 # Requisito 9
 def search_by_category(category):
     """Seu código deve vir aqui"""
+    return [
+        (new['title'], new['url']) for new in search_news(
+            {"category": {"$regex": category, "$options": "i"}}
+        )
+    ]
